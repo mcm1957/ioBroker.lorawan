69408d7702b8a100ba478e48242875a8882d5bc3
BenAhrdt <bsahrdt@gmail.com>
2024-01-07 09:33:33 +0100

    fist variable

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ class Lorawan extends utils.Adapter {
 		// this.config:
 		this.log.info("config option1: " + this.config.option1);
 		this.log.info("config option2: " + this.config.option2);
+		this.example = false;
 
 		/*
 		For every state in the system there has to be also an object of type state
